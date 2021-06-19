ab2bb784460e843f5b4b51bbb35de41256a6cc53
IkraamSage <82159115+IkraamSage@users.noreply.github.com>
2021-06-19 13:20:14 +0200

    Add files via upload

--- lotto.py
+++ lotto.py
@@ -1,117 +1,14 @@
-import random
 from tkinter import *
 from tkinter import ttk
 from tkinter import messagebox
-import random
-
-
 # GUI size and headers
 root = Tk()
 root.title("Sign Up")
 root.geometry("700x500")
 root.config(bg="yellow")
 
-# Add image file
-lotto = PhotoImage(file="lottery1.png")
-
-entry1 = Spinbox(root, from_=1, to=49, bg="red", font=("MS sans serif", 18))
-entry1.place(x=130, y=180, height=50, width=60)
-entry2 = Spinbox(root, from_=1, to=49, bg="green", font=("MS sans serif", 18))
-entry2.place(x=210, y=180, height=50, width=60)
-entry3 = Spinbox(root, from_=1, to=49, bg="blue", font=("MS sans serif", 18))
-entry3.place(x=290, y=180, height=50, width=60)
-entry4 = Spinbox(root, from_=1, to=49, bg="orange", font=("MS sans serif", 18))
-entry4.place(x=370, y=180, height=50, width=60)
-entry5 = Spinbox(root, from_=1, to=49, bg="purple", font=("MS sans serif", 18))
-entry5.place(x=460, y=180, height=50, width=60)
-entry6 = Spinbox(root, from_=1, to=49, bg="red", font=("MS sans serif", 18))
-entry6.place(x=530, y=180, height=50, width=60)
-
-def generator():
-    first = random.randint(1, 50)
-    ref1 = str(first)
-    ent1.set(ref1)
-
-    second = random.randint(1, 50)
-    ref2 = str(second)
-    ent2.set(ref2)
-
-    third = random.randint(1, 50)
-    ref3 = str(third)
-    ent3.set(ref3)
-
-    fourth = random.randint(1, 50)
-    ref4 = str(fourth)
-    ent4.set(ref4)
-
-    fifth = random.randint(1, 50)
-    ref5 = str(fifth)
-    ent5.set(ref5)
-
-    sixth = random.randint(1, 50)
-    ref6 = str(sixth)
-    ent6.set(ref6)
-
-    generatedList = [first, second, third, fourth, fifth, sixth]
-    inputList = [int(entry1.get()), int(entry2.get()), int(entry3.get()), int(entry4.get()), int(entry5.get()), int(entry6.get())]
-    matchList = list(set(generatedList).intersection(inputList))
-    num_match = len(matchList)
-    print(matchList)
-    print(num_match)
-    messagebox.showinfo(str(num_match))
-
-    if num_match <= 1:
-        messagebox.showinfo("No matching balls", "You have no winnings")
-    elif num_match == 2:
-        messagebox.showinfo("Well Done!", "You Have won R20")
-    elif num_match == 3:
-        messagebox.showinfo("Well Done!", "You have won R100.50")
-    elif num_match == 4:
-        messagebox.showinfo("Well Done!", "You Have Won R2384")
-    elif num_match == 5:
-        messagebox.showinfo("Well Done!", "You Have Won R8584")
-    else:
-        messagebox.showinfo("EXCELLENT!", "YOU'VE WON R10 000 000")
-
-def exit():
-    root.destroy()
-    import currency
-
-ent1 = StringVar()
-ent2 = StringVar()
-ent3 = StringVar()
-ent4 = StringVar()
-ent5 = StringVar()
-ent6 = StringVar()
-
-
-lotto_label = Label(root, bg="red", textvariable=ent1)
-lotto_label.place(x=130, y=350, height=50, width=50)
-lotto_label = Label(root, bg="green", textvariable=ent2)
-lotto_label.place(x=210, y=350, height=50, width=50)
-lotto_label = Label(root, bg="blue", textvariable=ent3)
-lotto_label.place(x=290, y=350, height=50, width=50)
-lotto_label = Label(root, bg="orange", textvariable=ent4)
-lotto_label.place(x=370, y=350, height=50, width=50)
-lotto_label = Label(root, bg="purple", textvariable=ent5)
-lotto_label.place(x=460, y=350, height=50, width=50)
-lotto_label = Label(root, bg="red", textvariable=ent6)
-lotto_label.place(x=530, y=350, height=50, width=50)
-# Show image using label
-image = Label(root, image=lotto)
-image.place(x=180, y=0)
-
 lucky_num = Label(root, text="Select your lucky numbers", font=("MS sans serif", 20), bg="yellow")
-lucky_num.place(x=180, y=120)
-
-play_button = Button(root, text="Play", command=generator)
-play_button.place(x=330, y=270)
-
-exit_btn = Button(root, text="exit")
-exit_btn.place(x=580, y=420)
-
-claim_button = Button(root, text="Claim Prize")
-claim_button.place(x=100, y=420)
+lucky_num.place(x=180, y=100)
 
 
 
--- main.py
+++ main.py
@@ -1,20 +1,18 @@
 from tkinter import *
 from tkinter import ttk
 from tkinter import messagebox
-import rsaidnumber, datetime
-from datetime import *
-import re
-import random
-
-
 # GUI size and headers
 root = Tk()
 root.title("Sign Up")
 root.geometry("550x400")
 root.config(bg="yellow")
 
+
+
+
 # Add image file
 lotto = PhotoImage(file="lottery1.png")
+
 
 
 # Show image using label
@@ -22,68 +20,33 @@ image = Label(root, image=lotto)
 image.place(x=100, y=0)
 
 
-lbl_name = Label(root, text="Enter Name:", font=("MS sans serif", 15), fg="black", bg="yellow")
+lbl_name = Label(root, text="Enter Name:", font=("MS sans serif", 20), fg="black", bg="yellow")
 lbl_name.place(x=50, y=150)
 
 txt_name = Entry(root)
 txt_name.place(x=300, y=150)
 
-lbl_email = Label(root, text="Email:", font=("MS sans serif", 15), fg="black", bg="yellow")
+lbl_email = Label(root, text="Email:", font=("MS sans serif", 20), fg="black", bg="yellow")
 lbl_email.place(x=50, y=200)
 
 txt_email = Entry(root)
 txt_email.place(x=300, y=200)
 
-address = Label(root, text="Address:", font=("MS sans serif", 15), fg="black", bg="yellow")
+address = Label(root, text="Address:", font=("MS sans serif", 20), fg="black", bg="yellow")
 address.place(x=50, y=250)
 
 address_entry = Entry(root)
 address_entry.place(x=300, y=250)
 
-id_number = Label(root, text="ID number:", font=("MS sans serif", 15), fg="black", bg="yellow")
+id_number = Label(root, text="ID number:", font=("MS sans serif", 20), fg="black", bg="yellow")
 id_number.place(x=50, y=300)
 
 id_number_entry = Entry(root)
 id_number_entry.place(x=300, y=300)
 
-def clear():
-    txt_name.delete(0, END)
-    txt_email.delete(0, END)
-    id_number_entry.delete(0, END)
-    address_entry.delete(0, END)
-
-def veri():
-    text_file = open("Text_file.txt", "a+")
-    text_file.write("Name: " + txt_name.get() + "Email: " + txt_email.get() + "Address: " + address_entry.get() + "Unique ID: " + id_number_entry.get())
-    text_file.close()
-    try:
-        id_number = rsaidnumber.parse(id_number_entry.get())
-        age = str((datetime.today() - id_number.date_of_birth) //
-              timedelta(days=365.25))
-        if int(age) >= 18:
-            messagebox.showinfo("verified", "Lets Play")
-        else:
-            messagebox.showinfo("not verified", "Must be over 18")
-    except ValueError:
-        messagebox.showerror("error", "Please enter valid ID number")
-
-        email_ = txt_email.get()
-        if re.search(verify, email_):
-            pass
-        else:
-            messagebox.showerror("error", "Invalid Email")
-            root.destroy()
-            import lotto
 
 
 
-verify_button = Button(root, text="Sign Up", command=veri)
-verify_button.place(x=400, y=350)
-
-verify = "^(\w|\.|\_|\-)+[@](\w|\_|\-.)+[.]\w{2,2}$"
-
-clear_button = Button(root, text="Clear", command=clear)
-clear_button.place(x=220, y=350)
 
 
 
